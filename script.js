d25bd5cee379fdce2a1370c72bf18c674963851d
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-07-08 15:31:57 +0100

    final functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 window.addEventListener("keydown", (e) => {
     
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(audio){  // if audio for key is present
         audio.currentTime = 0 //rewind audio to beginning
         audio.play() 
     }
-   
+   key.classList.add("playing") //class for the animation
 });
+
+const keys = document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener("transitionend", (e) =>{
+   if(e.propertyName === "transform"){
+    e.target.classList.remove('playing')
+   }
+}))
